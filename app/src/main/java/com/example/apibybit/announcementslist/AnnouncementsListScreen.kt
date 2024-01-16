98f2f89fca335d701fd93e6ac4f4ce2b7470d2d9
DivisionCom <xd6511@gmail.com>
2024-01-17 00:48:13 +0300

    Add main 'AnnouncementsList' Composable

--- app/src/main/java/com/example/apibybit/announcementslist/AnnouncementsListScreen.kt
+++ app/src/main/java/com/example/apibybit/announcementslist/AnnouncementsListScreen.kt
@@ -1,10 +1,13 @@
 package com.example.apibybit.announcementslist
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 
@@ -17,5 +20,14 @@ fun AnnouncementsListScreen(
         color = MaterialTheme.colorScheme.background,
         modifier = Modifier.fillMaxSize(),
     ) {
+        Spacer(modifier = Modifier.height(20.dp))
     }
+}
+
+@Composable
+fun AnnouncementsList(
+    navController: NavController,
+    viewModel: AnnouncementsListViewModel = hiltViewModel(),
+) {
+
 }
