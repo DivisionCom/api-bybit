a6f1d4534e370d67f4a5fab9d6d3e8d0387c282c
DivisionCom <xd6511@gmail.com>
2024-01-17 00:32:41 +0300

    Add loadAnnouncements function getting API response
    Add OkHttp3 Interceptor

--- app/src/main/java/com/example/apibybit/announcementslist/AnnouncementsListScreen.kt
+++ app/src/main/java/com/example/apibybit/announcementslist/AnnouncementsListScreen.kt
@@ -1,8 +1,12 @@
 package com.example.apibybit.announcementslist
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 
 @Composable
-fun AnnouncementsListScreen(navController: NavController) {
+fun AnnouncementsListScreen(
+    navController: NavController,
+    viewModel: AnnouncementsListViewModel = hiltViewModel(),
+) {
 }
--- app/src/main/java/com/example/apibybit/announcementslist/AnnouncementsListViewModel.kt
+++ app/src/main/java/com/example/apibybit/announcementslist/AnnouncementsListViewModel.kt
@@ -1,15 +1,38 @@
 package com.example.apibybit.announcementslist
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.apibybit.repository.BybitRepository
+import com.example.apibybit.util.Constants.API_LIMIT
+import com.example.apibybit.util.Constants.API_LOCALE
+import com.example.apibybit.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AnnouncementsListViewModel
-@Inject
-constructor(
-    private val repository: BybitRepository,
-) : ViewModel() {
+    @Inject
+    constructor(
+        private val repository: BybitRepository,
+    ) : ViewModel() {
+        var loadError = mutableStateOf("")
 
-}
+        init {
+            loadAnnouncements()
+        }
+
+        fun loadAnnouncements() {
+            viewModelScope.launch {
+                val result = repository.getAnnouncementsList(API_LOCALE, API_LIMIT)
+                when (result) {
+                    is Resource.Success -> {
+                    }
+                    is Resource.Error -> {
+                        loadError.value = result.message!!
+                    }
+                }
+            }
+        }
+    }
