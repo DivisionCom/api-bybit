a80df5cbd83e9dc89a1dcb39fdb1e32f37e7fdb4
DivisionCom <xd6511@gmail.com>
2024-01-16 22:11:08 +0300

    Add project's dark & light theme colors

--- app/src/main/java/com/example/apibybit/ui/theme/Color.kt
+++ app/src/main/java/com/example/apibybit/ui/theme/Color.kt
@@ -2,6 +2,8 @@ package com.example.apibybit.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val LightBlue = Color(0xFFBAC7FF)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
