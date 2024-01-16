d38529d8ab830e02ddb670f38134a68cc5e22f56
DivisionCom <xd6511@gmail.com>
2024-01-17 00:02:57 +0300

    Add constructor to ViewModel

--- app/src/main/java/com/example/apibybit/announcementslist/AnnouncementsListViewModel.kt
+++ app/src/main/java/com/example/apibybit/announcementslist/AnnouncementsListViewModel.kt
@@ -1,3 +1,15 @@
 package com.example.apibybit.announcementslist
 
+import androidx.lifecycle.ViewModel
+import com.example.apibybit.repository.BybitRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
 class AnnouncementsListViewModel
+@Inject
+constructor(
+    private val repository: BybitRepository,
+) : ViewModel() {
+
+}
