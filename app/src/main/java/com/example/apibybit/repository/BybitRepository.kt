65698b2f886b894843451c67c6512a256c3ed903
DivisionCom <xd6511@gmail.com>
2024-01-16 23:07:00 +0300

    Add constructor to repository & Add Resource class, Constants object

--- app/src/main/java/com/example/apibybit/repository/BybitRepository.kt
+++ app/src/main/java/com/example/apibybit/repository/BybitRepository.kt
@@ -1,6 +1,12 @@
 package com.example.apibybit.repository
 
+import com.example.apibybit.data.remote.BybitApi
 import dagger.hilt.android.scopes.ActivityScoped
+import javax.inject.Inject
 
 @ActivityScoped
 class BybitRepository
+    @Inject
+    constructor(
+        private val api: BybitApi,
+    )
---
+++ app/src/main/java/com/example/apibybit/util/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.apibybit.util
+
+object Constants
