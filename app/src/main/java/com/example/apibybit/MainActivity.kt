4a075dd934ad281e7148ff8f6aa0af478002e893
DivisionCom <xd6511@gmail.com>
2024-01-16 22:30:23 +0300

    Add Navigation Setup

--- app/src/main/java/com/example/apibybit/MainActivity.kt
+++ app/src/main/java/com/example/apibybit/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.apibybit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.apibybit.ui.theme.APIBybitTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,6 +13,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             APIBybitTheme {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "announcements_list_screen",
+                ) {
+                    composable("announcements_list_screen") {
+                    }
+                }
             }
         }
     }
