a9079be192fd662437bc3ee144f31d366922f75b
DivisionCom <xd6511@gmail.com>
2024-01-17 18:21:30 +0300

    Add clickable opening announcement URL in browser

--- app/src/main/java/com/example/apibybit/announcementslist/AnnouncementsListScreen.kt
+++ app/src/main/java/com/example/apibybit/announcementslist/AnnouncementsListScreen.kt
@@ -1,12 +1,17 @@
 package com.example.apibybit.announcementslist
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -17,6 +22,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -86,10 +94,18 @@ fun AnnouncementsEntry(
     modifier: Modifier = Modifier,
     viewModel: AnnouncementsListViewModel = hiltViewModel(),
 ) {
+    val uriHandler = LocalUriHandler.current
     Box(
         modifier =
             modifier
-                .clip(RoundedCornerShape(10.dp)),
+                .shadow(5.dp, RoundedCornerShape(10.dp))
+                .clip(RoundedCornerShape(10.dp))
+                .border(2.dp, Color.Yellow, RoundedCornerShape(10.dp))
+                .padding(8.dp)
+                .aspectRatio(1.5f)
+                .clickable {
+                    uriHandler.openUri(entry.url)
+                },
     ) {
         Column {
             Text(
@@ -99,10 +115,6 @@ fun AnnouncementsEntry(
             )
             Text(
                 text = entry.description,
-                fontSize = 14.sp,
-            )
-            Text(
-                text = entry.url,
                 fontSize = 14.sp,
             )
         }
